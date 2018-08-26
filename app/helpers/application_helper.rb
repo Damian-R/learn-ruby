3c7fb40700ec1520d648264e327cb67818ffb2d7
Damian Reiter <damian.reiter@gmail.com>
2018-08-26 17:53:19 +0000

    rails flavoured ruby

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def full_title(page_title = '')
+        full_title = "sample app"
+        if page_title.empty?
+            return full_title
+        else
+            return page_title + ' | ' + full_title
+        end
+    end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -8,7 +8,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   
   
   test "should get home" do
-    get static_pages_home_url
+    get root_url
     assert_response :success
     assert_select 'title', "home | #{@title_suffix}"
   end
